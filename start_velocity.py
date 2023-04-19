ec57d7bd4b01e83e1379b92f1c45d44c257b272d
sharifachegu <125981915+sharifachegu@users.noreply.github.com>
2023-04-19 11:36:23 +0300

    Create start_velocity.py

---
+++ start_velocity.py
@@ -0,0 +1,5 @@
+start_velocity = float(input("Введите начальную скорость (в м/с): "))
+end_velocity = float(input("Введите конечную скорость (в м/с): "))
+time = float(input("Введите время (в секундах): "))
+velocity = (start_velocity + end_velocity) / 2
+print("Скорость равна", velocity, "м/с")
